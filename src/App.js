6f41851a609ffb1f9a2a0c30d223df000ea360d5
reginaJeon <reginaJeon>
2023-09-24 11:17:11 +0900

    apply the React Router Dom

--- src/App.js
+++ src/App.js
@@ -5,41 +5,36 @@ import Banner from './components/Banner';
 import Row from './components/Row';
 import { requests } from './api/requests';
 import Footer from './components/Footer';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import DetailPage from './pages/DetailPage';
+import SearchPage from './pages/SearchPage';
+
+
+
+const Layout = () => {
+  return(
+    <div>
+      <Nav/>
+
+      <Outlet/>
+
+      <Footer/>
+    </div>
+  )
+}
+
 
 function App() {
-  console.log("app::::requests:::"+requests.fetchNetfilxOriginals)
   return (
-     /*className="App"*/
-    <div>
-      <Nav />
-      <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        id="NO"
-        fetchUrl={requests.fetchNetfilxOriginals}
-        isLargeRow
-      />
-      <Row
-        title="Trending Now"
-        id="TN"
-        fetchUrl={requests.fetchTrending}
-      />
-      <Row
-        title="Top Rated"
-        id="TR"
-        fetchUrl={requests.fetchTopRated}
-      />
-      <Row
-        title="Action"
-        id="AM"
-        fetchUrl={requests.fetchActionMovies}
-      />
-      <Row
-        title="Comedy"
-        id="CM"
-        fetchUrl={requests.fetchComedyMovies}
-      />
-      <Footer/>
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Layout/>}></Route>
+        <Route index element={<MainPage/>}></Route>
+        <Route path=':movieId' element={<DetailPage/>}></Route>
+        <Route index element={<SearchPage/>}></Route>
+      </Routes>
+
     </div>
   )
 }
---
+++ src/pages/MainPage/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Banner from '../../components/Banner';
+import Row from '../../components/Row';
+import { requests } from '../../api/requests';
+
+export default function MainPage() {
+  
+    console.log("app::::requests:::"+requests.fetchNetfilxOriginals);
+    return (
+    <div>        
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetfilxOriginals}
+        isLargeRow
+      />
+      <Row
+        title="Trending Now"
+        id="TN"
+        fetchUrl={requests.fetchTrending}
+      />
+      <Row
+        title="Top Rated"
+        id="TR"
+        fetchUrl={requests.fetchTopRated}
+      />
+      <Row
+        title="Action"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      />
+    </div>
+  )
+}
