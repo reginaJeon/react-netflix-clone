a2575e045e6964dc434e3acb1cdb64d22e503edf
reginaJeon <reginaJeon>
2023-10-16 19:53:48 +0900

    git

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
-        api_key: "f424705cf1c17158bc8cef56fda30c85",
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: "ko-KR"
     },
 });
